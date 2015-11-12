7d8c8a41e4b46531f2e7a4fc3667eeb0225bac25
guxiaobai <sikuan.gu@gmail.com>
2015-11-12 21:19:18 +0800

    update devise
    add devise_invitable
    add devise-i18n

--- cookbooks/devise/install.rb
+++ cookbooks/devise/install.rb
@@ -1,4 +1,14 @@
-generate "devise:install"
-inside 'config/locales' do
- run "curl -O https://gist.githubusercontent.com/Kenrick-Zhou/7909822/raw/6efca611800af99d1e918a43600c686c181e1a03/devise.zh-CN.yml"
-end
+# @TODO
+# 是否开启注册
+# 是否开启锁定
+# 是否添加用户名
+# 是否添加手机
+
+generate 'devise:install'
+generate 'devise_invitable:install'
+model_name = ask('please input model name')
+generate "devise #{model_name}"
+generate "devise_invitable #{model_name}"
+generate :migration, "AddNameTo#{model_name.capitalize} name:string:uniq"
+generate :migration, "AddMobileTo#{model_name.capitalize} mobile:string:uniq"
+rake "db:migrate"
