fb14b6e98b00ad30cfc675fa9b0e5a65ca541315
guxiaobai <sikuan.gu@gmail.com>
2015-11-15 11:49:35 +0800

    update devise

--- cookbooks/devise/install.rb
+++ cookbooks/devise/install.rb
@@ -4,28 +4,17 @@ generate 'devise_invitable:install'
 
 remove_file 'config/locales/devise.en.yml'
 
-model_name = ask('请输入用户表名(空白跳过): ')
+model_name = ask('请输入用户表名(空白跳过):')
 
 if  model_name.present?
   generate "devise #{model_name}"
   generate "devise_invitable #{model_name}"
-
-  if yes?('添加 name 字段? [Y/n]')
-      generate :migration, "AddNameTo#{model_name.capitalize} name:string:index"
-  end
-
-  if yes?("添加 mobile 字段 ? [Y/n]")
-      generate :migration, "AddMobileTo#{model_name.capitalize} mobile:string:index"
-  end
+  generate :migration, "AddNameTo#{model_name.capitalize} name:string:index"
+  generate :migration, "AddMobileTo#{model_name.capitalize} mobile:string:index"
 
   migrate_file = Dir["db/migrate/*"].grep(/.*_devise_create_#{model_name}.*/).first
-
-  if yes?('开启邮箱验证? [Y/n]')
-    uncomment_lines migrate_file, /.*confirm.*/
-  end
-
-  if yes?('开启账户锁定? [Y/n]')
-    uncomment_lines migrate_file, /.*unlock.*|.*failed_attempts.*/
-  end
+  uncomment_lines migrate_file, /.*confirm.*/
+  uncomment_lines migrate_file, /.*unlock.*|.*failed_attempts.*/
   rake "db:migrate"
+  
 end
