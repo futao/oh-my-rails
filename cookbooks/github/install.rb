860cfdbaeeea54805220450bb674e7f955f4eff6
guxiaobai <sikuan.gu@gmail.com>
2015-11-15 13:06:10 +0800

    update github

--- cookbooks/github/install.rb
+++ cookbooks/github/install.rb
@@ -1,9 +1,11 @@
 if yes?("Create a GitHub repository? (y/n)")
     run "hub create #{app_path.basename.to_s}"
-    run "hub push -u origin master"
     result = `hub remote -v | awk '{ print $2}' | head -n1`
     repository = result.strip
     gsub_file "config/deploy.rb", /git.*git/, repository
+    git add: '.'
+    git commit: %q{ -m 'oh-my-rails: add repo'}
+    run "hub push -u origin master"
 else
     #if github repo didn't create, then choose your own
     repository = ask("input your project's repo")
